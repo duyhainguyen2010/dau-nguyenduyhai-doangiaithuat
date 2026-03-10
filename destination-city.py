2eca24b56badb04dcba8c7d50e2cf10a23f4545e
Nguyễn Duy Hải <nguyenduyhai2010.dev@gmail.com>
2026-03-10 10:12:08 +0700

    Create destination-city.py

---
+++ destination-city.py
@@ -0,0 +1,13 @@
+class Solution:
+    def destCity(self, paths):
+
+        start_cities = set()
+
+        for path in paths:
+            start_cities.add(path[0])
+
+        for path in paths:
+            destination = path[1]
+
+            if destination not in start_cities:
+                return destination
