cc007ebd5a84bc88cd49e0bca6187c7b9cd0596b
Nguyễn Duy Hải <nguyenduyhai2010.dev@gmail.com>
2026-03-19 22:00:38 +0700

    Create count-common-words-with-one-occurrence.py

---
+++ count-common-words-with-one-occurrence.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countWords(self, words1, words2):
+        count = 0
+        for i in words1:
+            if words1.count(i) == 1 and words2.count(i) == 1:
+                count += 1
+        return count
+        
