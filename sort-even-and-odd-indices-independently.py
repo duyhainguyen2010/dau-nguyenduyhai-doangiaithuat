7e1688bf411e08c5bd5184b9b58891e59782d4e7
Nguyễn Duy Hải <nguyenduyhai2010.dev@gmail.com>
2026-03-23 21:59:46 +0700

    Create sort-even-and-odd-indices-independently.py

---
+++ sort-even-and-odd-indices-independently.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def sortEvenOdd(self, nums):
+        x = sorted(nums[::2])
+        y = sorted(nums[1::2], reverse=True)
+        nums[::2] = chan
+        nums[1::2] = le
+        return nums
