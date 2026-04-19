8bf18b60a9fe97b072d93f5a71101ac19c042fe5
Nguyễn Duy Hải <nguyenduyhai2010.dev@gmail.com>
2026-04-19 19:56:53 +0700

    Create climbing-stairs.py

---
+++ climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+
+        a = 1  
+        b = 2 
+
+        for i in range(3, n + 1):
+            c = a + b
+            a = b
+            b = c
+
+        return b
