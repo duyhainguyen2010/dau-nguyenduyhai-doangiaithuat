834e1cf302fa47432d51e172e29db583e0d4a06b
Nguyễn Duy Hải <nguyenduyhai2010.dev@gmail.com>
2026-04-20 23:07:12 +0700

    Create assign-cookies.py

---
+++ assign-cookies.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        
+        i = 0  # child pointer
+        j = 0  # cookie pointer
+        count = 0
+        
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                count += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+        
+        return count
